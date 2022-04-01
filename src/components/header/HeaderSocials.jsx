324274a69f989e937177a3464ec6bd116e67d8b4
Abed Bashir <56051101+AbedBashir@users.noreply.github.com>
2022-04-01 21:34:59 +0300

    Initial Commit added all the links to the socials and uploaded my cv and its now downloadable

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,10 +7,10 @@ import {BsFacebook} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
-        <a href="https://instagram.com" target="_blank"><BsInstagram/></a>
-        <a href="https://facebook.com" target="_blank"><BsFacebook/></a>
+        <a href="https://www.linkedin.com/in/abed-bashir-952732208/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/AbedBashir" target="_blank"><BsGithub/></a>
+        <a href="https://www.instagram.com/abedbashir/" target="_blank"><BsInstagram/></a>
+        <a href="https://www.facebook.com/abed.bachir" target="_blank"><BsFacebook/></a>
     </div>
   )
 }
